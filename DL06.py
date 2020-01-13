6f9f5a6abc1bf5a11f8635c2744ca65296276a84
Navak94 <44874063+Navak94@users.noreply.github.com>
2020-01-13 08:24:56 -0500

    Create DL06.py
    You likely will have to adjust height, etc for this to work on different screens

---
+++ DL06.py
@@ -0,0 +1,53 @@
+from pyModbusTCP.client import ModbusClient
+import time
+from tkinter import *
+from subprocess import call
+import sys
+#import pyvisa 
+SERVER_HOST = "192.168.72.114"
+SERVER_PORT = 502
+
+#have to adjust to fit your screen
+h=5 # Height
+w=10 #width
+#p=100# Height
+
+c = ModbusClient()
+c.host(SERVER_HOST) #192.168.72.114
+c.port(SERVER_PORT)
+
+toggle = False
+root=Tk()  
+root.attributes("-fullscreen",True) #fullscreen
+#root.geometry("1280x720") #windowed for debugging
+
+def quitpython():
+    exit()
+
+def shutdownraspi():
+    call("sudo nohup shutdown -h now", shell=True) # strictly works on raspbian or regular linux
+def RelayTrigger(ADDR): # take in the address
+    toggle = True
+    print(ADDR)
+    if not c.is_open():
+        if not c.open():
+            print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
+
+    if c.is_open():
+        is_ok = c.write_single_coil(ADDR, toggle) #give 2049 - 2057 a test as well, should trigger Y0 if it isn't tied to anything
+        time.sleep(1)
+        toggle = not toggle
+        is_ok = c.write_single_coil(ADDR, toggle)
+
+
+buttonPADC1006=Button(root, height=h, width=w, font =("Helvetica","40"),text="C1010", command=lambda: RelayTrigger(3593))
+buttonPADC1006.place(x=30,y=80)
+
+buttonPADCC1010=Button(root, height=h, width=w,font =("Helvetica","40"), text="C1005", command=lambda: RelayTrigger(3590))
+buttonPADCC1010.place(x=430,y=80)
+
+SHUTITALLDOWN=Button(root, height=2, width=w,font =("Helvetica","20"), text="Shut Down", command=lambda: shutdownraspi())
+SHUTITALLDOWN.place(x=560,y=400)
+
+QUITPYTHON=Button(root, height=2, width=w,font =("Helvetica","20"), text="Quit Python", command=lambda: quitpython())
+QUITPYTHON.place(x=30,y=400)
